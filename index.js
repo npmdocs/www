92255b089ca09a12904206589f97767d0bf87f7b
Eugene Sharygin <eush77@gmail.com>
2015-03-23 10:19:57 +0300

    Put a form as a fallback

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 'use strict';
 require('whatwg-fetch');
 
-var path = require('path'),
-    querystring = require('querystring').parse;
+const path = require('path'),
+      querystring = require('querystring').parse;
 
-var registry = 'http://npm-registry-cors-proxy.herokuapp.com/';
+const registry = 'http://npm-registry-cors-proxy.herokuapp.com/';
 
 
-var name = querystring(window.location.search.slice(1)).q;
+const query = querystring(window.location.search.slice(1)).q;
 
-window.fetch(path.join(registry, name))
-  .then((response) => response.json())
-  .then((info) => window.location = info.homepage);
+if (query) {
+  window.fetch(path.join(registry, name))
+    .then((response) => response.json())
+    .then((info) => {
+      document.write(`Redirecting to ${info.homepage}...`);
+      window.location = info.homepage;
+    });
+}
+else {
+  document.write('<form method="get"><input type="text" name="q"><input type="submit" value="Go"></form>');
+}
