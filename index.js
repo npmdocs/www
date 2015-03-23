0e2494f1b003d95ef86d2275bfebbab3cb2dec1e
Eugene Sharygin <eush77@gmail.com>
2015-03-23 11:07:52 +0300

    Change registry proxy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('whatwg-fetch');
 const path = require('path'),
       querystring = require('querystring').parse;
 
-const registry = 'http://npm-registry-cors-proxy.herokuapp.com/';
+const registry = 'http://npm-registry.herokuapp.com';
 
 
 const query = querystring(window.location.search.slice(1)).q;
